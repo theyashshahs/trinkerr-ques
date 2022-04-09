76acb83b53b18c42a3aadb52edd19474ea686fe1
theyashshahs <201851150@iiitvadodara.ac.in>
2022-04-09 10:59:38 +0530

    corrected code

--- ques2.py
+++ ques2.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     strings: list = input().split()
 
     res = []
+    visited = set()
 
     for str1 in strings:
         for str2 in strings:
@@ -16,7 +17,17 @@ if __name__ == "__main__":
                 continue
 
             if check(str1, str2):
-                res.append([str1, str2])
+                if not str1 in visited:
+                    res.append([str1, str2])
+
+                visited.add(str1)
+                visited.add(str2)
+
                 continue
+
+            else:
+                if not str1 in visited:
+                    res.append(str1)
+                visited.add(str1)
 
     print(res)
